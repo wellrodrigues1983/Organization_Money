f48a13c8ca15bfa1639019b70425ffd77ca3b9e8
Wellington Rodrigues <wellrodrigues@gmail.com>
2021-10-15 18:12:04 -0300

    Finalizado sliders e Activity cadstro e login.

--- app/src/main/java/com/wrsistemas/organizationmoney/MainActivity.java
+++ app/src/main/java/com/wrsistemas/organizationmoney/MainActivity.java
@@ -1,16 +1,59 @@
 package com.wrsistemas.organizationmoney;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.wrsistemas.organizationmoney.activity.CadastroActivity;
+import com.wrsistemas.organizationmoney.activity.LoginActivity;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build());
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build());
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                 .fragment(R.layout.intro_3)
+                .build());
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .build());
+
+        addSlide( new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_cadastro)
+                .build());
+    }
+
+    public void btEntrar(View view){
+
+        startActivity(new Intent(this, LoginActivity.class));
+
+    }
+
+    public void btCadastrar(View view){
+
+        startActivity(new Intent(this, CadastroActivity.class));
 
     }
 }
